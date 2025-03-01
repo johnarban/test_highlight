c44515446083e8c6fe596f977090e8ab1f188273
John Arban Lewis <johnarban@users.noreply.github.com>
2025-03-01 15:39:30 -0500

    all turning off highlights

--- test_highlight/components/PlotlyHighlighting.py
+++ test_highlight/components/PlotlyHighlighting.py
@@ -8,16 +8,20 @@ class _PlotlyHighlighting(VuetifyTemplate):
     template_file = os.path.abspath(os.path.join(os.path.dirname(__file__), "PlotlyHighlighting.vue"))
     viewer_id = Unicode().tag(sync=True)
     show = Bool(False).tag(sync=True)
+    highlight = Bool(True).tag(sync=True)
     
-    def __init__(self, viewer_id = '', *args, **kwargs):
+    def __init__(self, viewer_id = '', show = False, highlight = True, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.viewer_id = viewer_id
+        self.show = show
+        self.highlight = highlight
         pass
 
 
 @solara.component
-def PlotlyHighlighting():
-    _PlotlyHighlighting.element()
+def PlotlyHighlighting(viewer_id='', show=False, highlight=True):
+    return _PlotlyHighlighting(viewer_id=viewer_id, show=show, highlight=highlight)
+
     
     
 # @solara.component_vue('PlotlyHighlighting.vue')
